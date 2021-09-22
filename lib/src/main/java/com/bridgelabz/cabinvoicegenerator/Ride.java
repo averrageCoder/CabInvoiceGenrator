65e558affc6b47906fb6aaaf8a1d0375c6d7be53
Harsh R Jain <rjainharsh@gmail.com>
2021-09-22 18:44:05 +0530

    ADDED: type of ride field

--- lib/src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
+++ lib/src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -1,13 +1,18 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 public class Ride {
-
+	enum TypeOfRide {
+		NORMAL_RIDE, PREMIUM_RIDE
+	}
+	
 	int time;
 	double distance;
-
-	public Ride(double distance, int time) {
+	TypeOfRide typeOfRide;
+	
+	public Ride(double distance, int time, TypeOfRide type) {
 		this.distance = distance;
 		this.time = time;
+		this.typeOfRide = type;
 	}
 
 }
