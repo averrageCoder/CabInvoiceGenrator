e58f95dbfcc0f18e0a0dea0b1f6735809cb46d79
Harsh R Jain <rjainharsh@gmail.com>
2021-09-22 17:13:59 +0530

    REFACTED: constant created for minimum fare

--- lib/src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ lib/src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -4,11 +4,12 @@ public class InvoiceGenerator {
 
 	private static final double COST_PER_KM = 10;
 	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5;
 
 	public double calculateFare(double distance, int time) {
 		double fare = COST_PER_KM * distance + COST_PER_TIME * time;
-		if(fare < 5)
-			return 5;
+		if(fare < MINIMUM_FARE)
+			return MINIMUM_FARE;
 		else
 			return fare;
 	}
