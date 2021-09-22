6cc8cc2b3e5c0bb4d45e1e07651eabf5a3affefb
Harsh R Jain <rjainharsh@gmail.com>
2021-09-22 17:29:05 +0530

    ADDED: class to store individual rides

---
+++ lib/src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class Ride {
+
+	int time;
+	double distance;
+
+	public Ride(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+
+}
