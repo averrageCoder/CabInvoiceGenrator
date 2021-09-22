001822158efb8b03587384f316aae10d93f81eac
Harsh R Jain <rjainharsh@gmail.com>
2021-09-22 18:24:59 +0530

    ADDED: testcase for get invoive summary based on userID

--- lib/src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ lib/src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.api.Test;
 public class InvoiceServiceTest {
 	
 	InvoiceGenerator invoiceGenerator = null;
+	RideRepository rideRepository;
 	
 	@BeforeEach
 	public void setup() {
 		invoiceGenerator = new InvoiceGenerator();
+		rideRepository = new RideRepository();
 	}
 	
 	@Test
@@ -39,6 +41,26 @@ public class InvoiceServiceTest {
 				};
 		InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
 		InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+		assertEquals(expectedSummary, summary);
+	}
+	
+	@Test
+	public void givenUserID_shouldReturnInvoiceSummary() {
+		Ride[] ridesForUser1 = { 
+				new Ride(2.0, 5),
+				new Ride(0.1, 2)
+				};
+		rideRepository.addRidesForUser("101", ridesForUser1);
+		
+		Ride[] ridesForUser2 = { 
+			new Ride(5.0, 5),
+			new Ride(0.1, 2)
+		};
+		rideRepository.addRidesForUser("102", ridesForUser2);
+		
+		Ride[] rides = rideRepository.getRides("102");
+		InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+		InvoiceSummary expectedSummary = new InvoiceSummary(2, 60.0);
 		assertEquals(expectedSummary, summary);
 	}
 	
