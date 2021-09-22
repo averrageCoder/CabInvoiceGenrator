867bf74bdae0b72b0982afd5915b4fda1fb0628c
Harsh R Jain <rjainharsh@gmail.com>
2021-09-22 17:36:33 +0530

    REFACTORED: added a setup method to initialize global object

--- lib/src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ lib/src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -2,13 +2,21 @@ package com.bridgelabz.cabinvoicegenerator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
-
+	
+	InvoiceGenerator invoiceGenerator = null;
+	
+	@BeforeEach
+	public void setup() {
+		invoiceGenerator = new InvoiceGenerator();
+	}
+	
 	@Test
 	public void givenDistanceAndTime_shouldReturnTotalFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 2.0;
 		int time = 2;
 		double fare = invoiceGenerator.calculateFare(distance, time);
@@ -17,7 +25,6 @@ public class InvoiceServiceTest {
 	
 	@Test
 	public void givenLessDistanceAndTime_shouldReturnMinimumFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 0.1;
 		int time = 1;
 		double fare = invoiceGenerator.calculateFare(distance, time);
@@ -26,7 +33,6 @@ public class InvoiceServiceTest {
 	
 	@Test
 	public void givenMultipleRides_shouldReturnTotalFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		Ride[] rides = { 
 				new Ride(2.0, 5),
 				new Ride(0.1, 2)
